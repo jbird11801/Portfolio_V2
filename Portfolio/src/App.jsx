c1d62cdd948054f2d81f4e1f1226c15152b32d45
jbird11801@gmail.com <jbird11801@gmail.com>
2023-11-18 12:11:53 -0500

    feature/about

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,10 +1,18 @@
 import Nav from './componets/nav'
 
+import { Outlet } from 'react-router-dom';
+
 function App() {
 
   return (
+    
+    <>
 
    <Nav/>
+
+   <Outlet/>
+
+   </>
 
   )
 
---
+++ Portfolio/src/pages/About.jsx
@@ -0,0 +1,19 @@
+export default function About() {
+
+    return (
+
+        <div className="mx-6">
+
+            <h1 className="is-size-2-desktop is-size-5-touch has-text-weight-bold is-underlined">About Me</h1>
+
+                <p className="is-size-4-desktop is-size-7-touch">
+
+                Hi everyone, my name is Jarod Fredette! I love playing games and programming– sometimes a mix of the two if I’m making a mod! I enjoy learning about computers and working with technology, and hope to continue doing so for a long time. I strive to create environments where I can constistently grow, communicate, help others, and succeed as a team. In my day to day life, I maintain a positive mental attitude– especially in high stress environments. I keep a positive work space. I enjoy practicing clear communication skills, and when I work with a group, I hope everyone leaves knowing more and in a positive mood.
+
+                </p>
+
+        </div>
+
+    )
+
+}
